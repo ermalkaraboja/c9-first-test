82415dd320134e7e2c20d6989f6cd9e2f789a83e
Ermal Karaboja <ermalkaraboja@gmail.com>
2016-11-01 11:16:55 +0000

    initial commit

---
+++ angular2-first-test/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component }        from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h2>My First Title from the coomponent</h2>'
+})
+
+export class AppComponent { }
